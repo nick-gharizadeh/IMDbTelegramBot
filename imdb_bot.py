853274e210b33181a62177f66c6cc74007211450
Nick Gharizadeh <nickgharizadeh@gmail.com>
2023-03-15 10:42:52 +0330

    fea/ start command

--- imdb_bot.py
+++ imdb_bot.py
@@ -1,2 +1,13 @@
+from telegram.ext import Updater, CommandHandler, Application
+from bot_token import token
 
 
+async def start_command(update, context):
+    await update.message.reply_text('Hi! We\'re glad to have you here! 😉 \n'
+                                    'Let\'s get started with some amazing features! 💛')
+
+
+if __name__ == '__main__':
+    application = Application.builder().token(token).build()
+    application.add_handler(CommandHandler('start', start_command))
+    application.run_polling(1.0)
