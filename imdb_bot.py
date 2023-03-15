8e46505c6b0aff42b0e8da89972ffdbac72f50e8
Nick Gharizadeh <nickgharizadeh@gmail.com>
2023-03-15 15:10:43 +0330

    fea/ show name of movies that user search

--- imdb_bot.py
+++ imdb_bot.py
@@ -1,5 +1,8 @@
-from telegram.ext import Updater, CommandHandler, Application
+from telegram.ext import Updater, CommandHandler, Application, MessageHandler, filters
 from bot_token import token
+import imdb
+
+imdb = imdb.IMDb()
 
 
 async def start_command(update, context):
@@ -7,7 +10,17 @@ async def start_command(update, context):
                                     '\nLet\'s get started with some amazing features! 💛')
 
 
+async def reply_message(update, context):
+    movie_name = update.message.text
+    search = imdb.search_movie(movie_name)
+    result = " "
+    for i in range(0, len(search)):
+        result += f"{search[i]} \n"
+    await update.message.reply_text(result)
+
+
 if __name__ == '__main__':
     application = Application.builder().token(token).build()
     application.add_handler(CommandHandler('start', start_command))
+    application.add_handler(MessageHandler(filters.TEXT, reply_message))
     application.run_polling(1.0)
