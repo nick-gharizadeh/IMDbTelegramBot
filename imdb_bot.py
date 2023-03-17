a01b2ea9ddcc591ca09145f0e8cdea0726019732
Nick Gharizadeh <nickgharizadeh@gmail.com>
2023-03-17 16:07:49 +0330

    fea/ show name of movies that user search as button to know what did the movie that user choose

--- imdb_bot.py
+++ imdb_bot.py
@@ -1,8 +1,14 @@
+import telegram
+from telebot.types import ReplyKeyboardRemove
+from telegram import KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, Application, MessageHandler, filters
 from bot_token import token
 import imdb
+import telebot
 
 imdb = imdb.IMDb()
+bot = telebot.TeleBot(token)
+buttons = []
 
 
 async def start_command(update, context):
@@ -12,11 +18,28 @@ async def start_command(update, context):
 
 async def reply_message(update, context):
     movie_name = update.message.text
-    search = imdb.search_movie(movie_name)
-    result = " "
-    for i in range(0, len(search)):
-        result += f"{search[i]} \n"
-    await update.message.reply_text(result)
+    global buttons
+    if len(buttons) != 0 and is_movie_exist_on_keyboard(movie_name):
+        await show_movie_details(update, context)
+    else:
+        buttons.clear()
+        search = imdb.search_movie(movie_name)
+        for i in range(0, len(search)):
+            buttons.append([KeyboardButton(f"🎬 {search[i]}")])
+        await update.message.reply_text("Now, select one of these results to show the details:",
+                                        reply_markup=ReplyKeyboardMarkup(buttons))
+
+
+async def show_movie_details(update, context):
+    await update.message.reply_text(update.message.text)
+
+
+def is_movie_exist_on_keyboard(movie_name):
+    for button in buttons:
+        for button_items in button:
+            if button_items.text == movie_name:
+                return True
+    return False
 
 
 if __name__ == '__main__':
